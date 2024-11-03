6741b70bc3244edb5493482558da07f1842d6a1d
theon87 <theonfrancis17@gmail.com>
2024-11-03 00:55:18 -0400

    update performactions response for a motorbike to perform the wheelie action

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -407,6 +407,13 @@ class Cli {
           return;
         }
         // TODO: add statements to perform the wheelie action only if the selected vehicle is a motorbike
+        else if (answers.action === 'Wheelie') {
+          for (let i = 0; i < this.vehicles.length; i++) {
+            if (this.vehicles[i].vin === this.selectedVehicleVin && this.vehicles[i] instanceof Motorbike) {
+              (this.vehicles[i] as Motorbike).wheelie();
+            }
+          }
+        }
         else if (answers.action === 'Select or create another vehicle') {
           // start the cli to return to the initial prompt if the user wants to select or create another vehicle
           this.startCli();
