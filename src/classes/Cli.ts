bf8a549c19c2aa2826c25b5d207a0e76cad3765a
theon87 <theonfrancis17@gmail.com>
2024-11-03 00:03:18 -0400

    add options to performActions prompt to tow and wheelie

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -331,6 +331,8 @@ class Cli {
             'Turn right',
             'Turn left',
             'Reverse',
+            'Tow',
+            'Wheelie',
             'Select or create another vehicle',
             'Exit',
           ],
